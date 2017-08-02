28fd654cda983ec2e5ab9f5345dc557b8a5d3583
tamier <m2ta@uwaterloo.ca>
2017-08-02 15:25:30 -0400

    Print contradicting constraints if inference failed

--- src/maxsatbackend/MaxSatBackEnd.java
+++ src/maxsatbackend/MaxSatBackEnd.java
@@ -1,26 +1,5 @@
 package maxsatbackend;
 
-import org.checkerframework.framework.type.QualifierHierarchy;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.*;
-
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.AnnotationMirror;
-
-import org.sat4j.core.VecInt;
-import org.sat4j.maxsat.SolverFactory;
-import org.sat4j.maxsat.WeightedMaxSatDecorator;
-import org.sat4j.specs.ISolver;
-import org.sat4j.tools.xplain.DeletionStrategy;
-import org.sat4j.tools.xplain.Xplain;
-
-import util.MathUtils;
-import util.StatisticPrinter;
-import util.StatisticPrinter.StatisticKey;
-import util.VectorUtils;
 import checkers.inference.InferenceMain;
 import checkers.inference.SlotManager;
 import checkers.inference.model.Constraint;
@@ -29,6 +8,31 @@ import checkers.inference.model.Serializer;
 import checkers.inference.model.Slot;
 import constraintsolver.BackEnd;
 import constraintsolver.Lattice;
+import org.checkerframework.framework.type.QualifierHierarchy;
+import org.sat4j.core.VecInt;
+import org.sat4j.maxsat.WeightedMaxSatDecorator;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
+import util.MathUtils;
+import util.PrintUtils;
+import util.StatisticPrinter;
+import util.StatisticPrinter.StatisticKey;
+import util.VectorUtils;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author jianchu MaxSat back end converts constraints to VecInt, and solves
@@ -37,16 +41,11 @@ import constraintsolver.Lattice;
 public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
 
     protected final SlotManager slotManager;
-    protected final Map<Integer, Constraint> clauseConstraintMap = new HashMap<>();
     protected final List<VecInt> hardClauses = new LinkedList<VecInt>();
+    protected final List<Constraint> hardConstraints;
     protected final List<VecInt> softClauses = new LinkedList<VecInt>();
     protected final File CNFData = new File(new File("").getAbsolutePath() + "/cnfData");
     protected StringBuilder CNFInput = new StringBuilder();
-
-    // Whether we should compute the minimum unsatisfiable constraints
-    // and output a more useful error in the case of an unsolvable set of constraints.
-    private boolean explainUnsolvable;
-
     private long serializationStart;
     private long serializationEnd;
     protected long solvingStart;
@@ -59,12 +58,11 @@ public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
         super(configuration, slots, constraints, qualHierarchy, processingEnvironment, realSerializer,
                 lattice);
         this.slotManager = InferenceMain.getInstance().getSlotManager();
+        this.hardConstraints = new LinkedList<>();
 
         if (shouldOutputCNF()) {
             CNFData.mkdir();
         }
-
-        explainUnsolvable = "true".equals(configuration.get("explain"));
     }
 
     protected boolean shouldOutputCNF() {
@@ -127,9 +125,7 @@ public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
                         softClauses.add(res);
                     } else {
                         hardClauses.add(res);
-                        if (explainUnsolvable) {
-                            clauseConstraintMap.put(hardClauses.size(), constraint);
-                        }
+                        hardConstraints.add(constraint);
                     }
                 }
             }
@@ -196,13 +192,6 @@ public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
                 solver.addHardClause(hardClause);
             }
 
-            // We need the clauses to create the solver for an
-            // explanation in the case of a failure
-            if (!explainUnsolvable) {
-                // saving memory of JVM...
-                this.hardClauses.clear();
-            }
-
             for (VecInt softclause : softClauses) {
                 solver.addSoftClause(softclause);
             }
@@ -226,58 +215,31 @@ public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
             }
 
             if (isSatisfiable) {
-                // saving memory of JVM...
-                this.softClauses.clear();
                 result = decode(solver.model());
-                // PrintUtils.printResult(result);
+                PrintUtils.printResult(result);
             } else {
-                System.out.println("Not solvable!");
-                if (explainUnsolvable) {
-
-                    // Unfortunately we can't use our existing solver
-                    // since the Xplain framework doesn't work with WeightedMaxSatDecorator.
-                    // Thus, in the hopefully rare case of encountering an unsolvable set of contraints
-                    // we create a new regular solver instance and feed it just the hard clauses.
-                    Xplain<ISolver> xplainer = new Xplain<ISolver>(SolverFactory.newDefault());
-                    xplainer.setMinimizationStrategy(new DeletionStrategy());
-                    configureSatSolver(xplainer);
-
-                    for (VecInt clause : this.hardClauses) {
-                        xplainer.addClause(clause);
-                    }
-
-                    if (!xplainer.isSatisfiable()) {
-                        int[] indicies = xplainer.minimalExplanation();
-                        Set<Constraint> seenConstraints = new HashSet<>();
-
-                        System.out.println("Inference failed because of the following inconsistent constraints:");
-
-                        for (int clause : indicies) {
-                            Constraint constraint = clauseConstraintMap.get(clause);
-                            if (constraint == null || seenConstraints.contains(constraint)) {
-                                continue;
-                            }
-
-                            System.out.println("\t" + constraint + " @ " + constraint.getLocation().toString());
-
-                            seenConstraints.add(constraint);
-                        }
-                    } else {
-                        // We really shouldn't ever hit this case as we've already determined the
-                        // original instance was unsolvable. So too should xplainer.
-                    }
-
-                    System.exit(2);
-                }
+                int numberOfNewVars = slotManager.getNumberOfSlots() * lattice.numTypes;
+                int numberOfClauses = hardClauses.size();
+                PrintUtils.printContradictingHardConstraints(hardClauses, hardConstraints, numberOfNewVars, numberOfClauses);
             }
-
-        } catch (Throwable e) {
-            e.printStackTrace();
+        } catch (ContradictionException e) {
+            int numberOfNewVars = slotManager.getNumberOfSlots() * lattice.numTypes;
+            int numberOfClauses = hardClauses.size();
+            PrintUtils.printContradictingHardConstraints(hardClauses, hardConstraints, numberOfNewVars, numberOfClauses);
+        } catch (TimeoutException e) {
+            throw new RuntimeException("Failed to solve constrains due to timeout", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Unknown exception occurred when solving constraints", e);
+        } finally {
+            // saving memory of JVM...
+            this.hardClauses.clear();
+            this.softClauses.clear();
+            this.constraints = null;
         }
-        // saving memory of JVM...
-        this.constraints = null;
+
         return result;
     }
+
 
     /**
      * sat solver configuration Configure
@@ -297,27 +259,12 @@ public class MaxSatBackEnd extends BackEnd<VecInt[], VecInt[]> {
 
     protected void countVariables() {
 
-        Set<Integer> vars = new HashSet<Integer>();
+        Set<Integer> vars = new HashSet<>();
         for (VecInt vi : hardClauses) {
             for (int i : vi.toArray()) {
                 vars.add(i);
             }
         }
         StatisticPrinter.record(StatisticKey.CNF_VARIABLE_SIZE, (long) vars.size());
-    }
-
-    /**
-     * print all soft and hard clauses for testing.
-     */
-    protected void printClauses() {
-        System.out.println("Hard clauses: ");
-        for (VecInt hardClause : hardClauses) {
-            System.out.println(hardClause);
-        }
-        System.out.println();
-        System.out.println("Soft clauses: ");
-        for (VecInt softClause : softClauses) {
-            System.out.println(softClause);
-        }
     }
 }
